93f7ff99941dad348fc1a09620f11b85d852d9ff
Koji Nishi <koji.nishi@tomtom.com>
2022-04-07 22:34:36 +0900

    Add new py by Koji

---
+++ nishiSample.py
@@ -0,0 +1,19 @@
+
+def getEvenList(targetNo:int):
+    lst = []
+    for i in range(1,targetNo):
+        if i % 2 == 0:
+            lst.append(i)
+    return lst
+
+
+if __name__ == '__main__':
+    
+    targetNumber = input("input : Target Number -> ")
+
+    while targetNumber.isnumeric() == False :
+        targetNumber = input("input : Target Number -> ")
+    
+    evenList = getEvenList(int(targetNumber))
+
+    print(evenList)
